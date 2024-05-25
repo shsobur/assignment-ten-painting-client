fea5955bb053d61942f3fdb7824a5d5df736e9f6
shsobur <shsoburhossen951@gmail.com>
2024-05-25 16:25:53 +0600

    addCraft data sent to database fintsh

--- src/Pages/AddCraft.jsx
+++ src/Pages/AddCraft.jsx
@@ -1,10 +1,65 @@
+import Swal from "sweetalert2";
 import "../Styles/AddCraft.css";
 
 const AddCraft = () => {
+  const handelAddCraft = (event) => {
+    event.preventDefault();
+
+    const form = event.target;
+
+    const image = form.image.value;
+    const itme_name = form.itme_name.value;
+    const subcategory_name = form.subcategory_name.value;
+    const price = form.price.value;
+    const rating = form.rating.value;
+    const customization = form.customization.value;
+    const processing_time = form.processing_time.value;
+    const stock_status = form.stock_status.value;
+    const user_name = form.user_name.value;
+    const user_email = form.user_email.value;
+    const description = form.description.value;
+
+    const newCraft = {
+      image,
+      itme_name,
+      subcategory_name,
+      price,
+      rating,
+      customization,
+      processing_time,
+      stock_status,
+      user_name,
+      user_email,
+      description,
+    };
+
+    console.log(newCraft);
+
+    fetch("http://localhost:5000/addcraft", {
+      method: "POST",
+      headers: {
+        "content-type": "application/json",
+      },
+      body: JSON.stringify(newCraft),
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+        if (data.insertedId) {
+          Swal.fire({
+            title: "success!",
+            text: "Add Craft successfully",
+            icon: "success",
+            confirmButtonText: "Cool",
+          });
+          form.reset();
+        }
+      });
+  };
+
   return (
     <div className="main_addcraft_container">
       <div className="main_inner_addcraft_container">
-
         <div className="main_addcraft_input_container">
           <div className="addcraft_title_container">
             <h2>Add Craft Item</h2>
@@ -18,7 +73,7 @@ const AddCraft = () => {
           </div>
 
           <div>
-            <form>
+            <form onSubmit={handelAddCraft}>
               <div className="addcraft_input_form_container">
                 <div
                   className="addcraft_inputs"
@@ -27,7 +82,7 @@ const AddCraft = () => {
                   data-aos-duration="1000"
                 >
                   <p>Image</p>
-                  <input type="text" placeholder="URL" />
+                  <input type="text" name="image" placeholder="URL" />
                 </div>
 
                 <div
@@ -37,7 +92,7 @@ const AddCraft = () => {
                   data-aos-duration="1000"
                 >
                   <p>Item name</p>
-                  <input type="text" />
+                  <input type="text" name="itme_name" />
                 </div>
 
                 <div
@@ -47,7 +102,7 @@ const AddCraft = () => {
                   data-aos-duration="1000"
                 >
                   <p>Subcategory name</p>
-                  <input type="text" />
+                  <input type="text" name="subcategory_name" />
                 </div>
 
                 <div
@@ -57,7 +112,7 @@ const AddCraft = () => {
                   data-aos-duration="1000"
                 >
                   <p>Price</p>
-                  <input type="text" />
+                  <input type="text" name="price" />
                 </div>
 
                 <div
@@ -67,7 +122,7 @@ const AddCraft = () => {
                   data-aos-duration="1000"
                 >
                   <p>Rating</p>
-                  <input type="text" />
+                  <input type="text" name="rating" />
                 </div>
 
                 <div
@@ -77,7 +132,11 @@ const AddCraft = () => {
                   data-aos-duration="1000"
                 >
                   <p>Customization</p>
-                  <input type="text" placeholder="yes / no" />
+                  <input
+                    type="text"
+                    name="customization"
+                    placeholder="yes / no"
+                  />
                 </div>
 
                 <div
@@ -87,7 +146,7 @@ const AddCraft = () => {
                   data-aos-duration="1000"
                 >
                   <p>Processing time</p>
-                  <input type="text" />
+                  <input type="text" name="processing_time" />
                 </div>
 
                 <div
@@ -97,7 +156,7 @@ const AddCraft = () => {
                   data-aos-duration="1000"
                 >
                   <p>Stock status</p>
-                  <input type="text" />
+                  <input type="text" name="stock_status" />
                 </div>
 
                 <div
@@ -107,7 +166,7 @@ const AddCraft = () => {
                   data-aos-duration="1000"
                 >
                   <p>User name</p>
-                  <input type="text" />
+                  <input type="text" name="user_name" />
                 </div>
 
                 <div
@@ -117,7 +176,7 @@ const AddCraft = () => {
                   data-aos-duration="1000"
                 >
                   <p>User email</p>
-                  <input type="text" />
+                  <input type="text" name="user_email" />
                 </div>
               </div>
 
@@ -129,7 +188,11 @@ const AddCraft = () => {
               >
                 <div className="last_addcraft_inputs">
                   <p>Short description</p>
-                  <input type="text" placeholder="write hear..." />
+                  <input
+                    type="text"
+                    name="description"
+                    placeholder="description of image"
+                  />
                 </div>
 
                 <div className="form_btn_container">
@@ -143,7 +206,6 @@ const AddCraft = () => {
             </form>
           </div>
         </div>
-
       </div>
     </div>
   );
