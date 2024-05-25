0854f20ec44d79a1c1b9b7cdf6ef18878acf5d99
shsobur <shsoburhossen951@gmail.com>
2024-05-25 11:33:18 +0600

    added some animation on sign in and up page

--- src/Pages/AddCraft.jsx
+++ src/Pages/AddCraft.jsx
@@ -6,7 +6,6 @@ const AddCraft = () => {
       <div className="main_inner_addcraft_container">
 
         <div className="main_addcraft_input_container">
-
           <div className="addcraft_title_container">
             <h2>Add Craft Item</h2>
             <img src="https://i.ibb.co/YQNz8tD/5684260c0da46cd945aa0c23-separate-1.png" />
@@ -20,61 +19,114 @@ const AddCraft = () => {
 
           <div>
             <form>
-
               <div className="addcraft_input_form_container">
-                <div className="addcraft_inputs">
+                <div
+                  className="addcraft_inputs"
+                  data-aos="fade-right"
+                  data-aos-easing="ease-out-cubic"
+                  data-aos-duration="1000"
+                >
                   <p>Image</p>
                   <input type="text" placeholder="URL" />
                 </div>
 
-                <div className="addcraft_inputs">
+                <div
+                  className="addcraft_inputs"
+                  data-aos="fade-left"
+                  data-aos-easing="ease-out-cubic"
+                  data-aos-duration="1000"
+                >
                   <p>Item name</p>
                   <input type="text" />
                 </div>
 
-                <div className="addcraft_inputs">
+                <div
+                  className="addcraft_inputs"
+                  data-aos="fade-right"
+                  data-aos-easing="ease-out-cubic"
+                  data-aos-duration="1000"
+                >
                   <p>Subcategory name</p>
                   <input type="text" />
                 </div>
 
-                <div className="addcraft_inputs">
+                <div
+                  className="addcraft_inputs"
+                  data-aos="fade-left"
+                  data-aos-easing="ease-out-cubic"
+                  data-aos-duration="1000"
+                >
                   <p>Price</p>
                   <input type="text" />
                 </div>
 
-                <div className="addcraft_inputs">
+                <div
+                  className="addcraft_inputs"
+                  data-aos="fade-right"
+                  data-aos-easing="ease-out-cubic"
+                  data-aos-duration="1000"
+                >
                   <p>Rating</p>
                   <input type="text" />
                 </div>
 
-                <div className="addcraft_inputs">
+                <div
+                  className="addcraft_inputs"
+                  data-aos="fade-left"
+                  data-aos-easing="ease-out-cubic"
+                  data-aos-duration="1000"
+                >
                   <p>Customization</p>
                   <input type="text" placeholder="yes / no" />
                 </div>
 
-                <div className="addcraft_inputs">
+                <div
+                  className="addcraft_inputs"
+                  data-aos="fade-right"
+                  data-aos-easing="ease-out-cubic"
+                  data-aos-duration="1000"
+                >
                   <p>Processing time</p>
                   <input type="text" />
                 </div>
 
-                <div className="addcraft_inputs">
+                <div
+                  className="addcraft_inputs"
+                  data-aos="fade-left"
+                  data-aos-easing="ease-out-cubic"
+                  data-aos-duration="1000"
+                >
                   <p>Stock status</p>
                   <input type="text" />
                 </div>
 
-                <div className="addcraft_inputs">
+                <div
+                  className="addcraft_inputs"
+                  data-aos="fade-right"
+                  data-aos-easing="ease-out-cubic"
+                  data-aos-duration="1000"
+                >
                   <p>User name</p>
                   <input type="text" />
                 </div>
 
-                <div className="addcraft_inputs">
+                <div
+                  className="addcraft_inputs"
+                  data-aos="fade-left"
+                  data-aos-easing="ease-out-cubic"
+                  data-aos-duration="1000"
+                >
                   <p>User email</p>
                   <input type="text" />
                 </div>
               </div>
 
-              <div className="additional_input_section">
-
+              <div
+                className="additional_input_section"
+                data-aos="zoom-in"
+                data-aos-easing="ease-out-cubic"
+                data-aos-duration="1000"
+              >
                 <div className="last_addcraft_inputs">
                   <p>Short description</p>
                   <input type="text" placeholder="write hear..." />
@@ -83,15 +135,11 @@ const AddCraft = () => {
                 <div className="form_btn_container">
                   <label htmlFor="105">
                     <button className="addcraft_btn">
-
                       <input type="submit" value="Add" />
-
                     </button>
                   </label>
                 </div>
-
               </div>
-
             </form>
           </div>
         </div>
